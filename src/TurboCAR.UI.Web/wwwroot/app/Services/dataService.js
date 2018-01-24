02e325c7046abc2f5032ece0c4af527778b1ee7e
Niraj Ram Shivaram <nirajram.shivaram@mindtree.com>
2018-01-24 16:41:26 +0000

    Setting up in server

--- src/TurboCAR.UI.Web/wwwroot/app/Services/dataService.js
+++ src/TurboCAR.UI.Web/wwwroot/app/Services/dataService.js
@@ -14,30 +14,30 @@
         return service;
 
         function getCARs() {
-            return $http.get('http://a2svbml14011:8089/api/CAR');
+            return $http.get('http://localhost:8089/api/CAR');
             //return $http.get('http://localhost:18706/api/CAR');
         };
 
         function getBorrower()
         {
-            return $http.get('http://a2svbml14011:8061/api/Borrower');
+            return $http.get('http://localhost:8061/api/Borrower');
             //return $http.get('http://13.67.238.64:5003/api/Borrower');
         }
         function getTeams()
         {
-            return $http.get('http://a2svbml14011:8064/api/Team');
+            return $http.get('http://localhost:8064/api/Team');
            // return $http.get('http://13.67.238.64:5002/api/Team');
         }
         function getTeamMembers(teamId)
         {
-            return $http.get('http://a2svbml14011:8063/api/Employee/' + teamId);
+            return $http.get('http://localhost:8063/api/Employee/' + teamId);
             //return $http.get('http://13.67.238.64:5004/api/Employee/' + teamId );
         }
         function addCAR(CAR) {
-            return $http.post('http://a2svbml14011:8062/api/CAR', CAR);
+            return $http.post('http://localhost:8062/api/CAR', CAR);
             //return $http.post('http://13.67.238.64:5000/api/CAR', CAR);
         };
-
+        
         
     }
     return dataService;
